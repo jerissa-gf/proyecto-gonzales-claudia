bd48cc0a2527a1d18306ce16fefaa2e0b3215999
gonzales-jerissa <clau.yeri@gmail.com>
2025-05-11 20:03:51 -0400

    Despliegue entorno dev

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,86 +8,18 @@ import { Product } from '../models/product';
 export class ProductService {
 
   private productos: Product[] = [
-    {
-      id: 1,
-      imagen: 'assets/images/leche-condensada-pil.webp',
-      mensaje: 'LECHE CONDENSADA',
-      precio: 12,
-      descripcion: 'PRODUCTO LÁCTEO'
-    },
-    {
-      id: 2, 
-      imagen: 'assets/images/dulceDeLeche.jpg',
-      mensaje: 'DULCE DE LECHE', 
-      precio: 16, 
-      descripcion: 'PRODUCTO LÁCTEO'},
-    {
-      id: 3, 
-      imagen: 'assets/images/lecheEvaporada.webp',
-      mensaje: 'LECHE EVAPORADA', 
-      precio: 11, 
-      descripcion: 'PRODUCTO LÁCTEO'
-    },
-    {
-      id: 4,
-      imagen: 'assets/images/leche-condensada-pil.webp',
-      mensaje: 'LECHE CONDENSADA',
-      precio: 12,
-      descripcion: 'PRODUCTO LÁCTEO'
-    },
-    {
-      id: 5, 
-      imagen: 'assets/images/dulceDeLeche.jpg',
-      mensaje: 'DULCE DE LECHE', 
-      precio: 16, 
-      descripcion: 'PRODUCTO LÁCTEO'},
-    {
-      id: 6, 
-      imagen: 'assets/images/lecheEvaporada.webp',
-      mensaje: 'LECHE EVAPORADA', 
-      precio: 11, 
-      descripcion: 'PRODUCTO LÁCTEO'
-    },
-    {
-      id: 7,
-      imagen: 'assets/images/leche-condensada-pil.webp',
-      mensaje: 'LECHE CONDENSADA',
-      precio: 12,
-      descripcion: 'PRODUCTO LÁCTEO'
-    },
-    {
-      id: 8, 
-      imagen: 'assets/images/dulceDeLeche.jpg',
-      mensaje: 'DULCE DE LECHE', 
-      precio: 16, 
-      descripcion: 'PRODUCTO LÁCTEO'},
-    {
-      id: 9, 
-      imagen: 'assets/images/lecheEvaporada.webp',
-      mensaje: 'LECHE EVAPORADA', 
-      precio: 11, 
-      descripcion: 'PRODUCTO LÁCTEO'
-    },
-    {
-      id: 10,
-      imagen: 'assets/images/leche-condensada-pil.webp',
-      mensaje: 'LECHE CONDENSADA',
-      precio: 12,
-      descripcion: 'PRODUCTO LÁCTEO'
-    },
-    {
-      id: 11, 
-      imagen: 'assets/images/dulceDeLeche.jpg',
-      mensaje: 'DULCE DE LECHE', 
-      precio: 16, 
-      descripcion: 'PRODUCTO LÁCTEO'},
-    {
-      id: 12, 
-      imagen: 'assets/images/lecheEvaporada.webp',
-      mensaje: 'LECHE EVAPORADA', 
-      precio: 11, 
-      descripcion: 'PRODUCTO LÁCTEO'
-    }
+    {id: 1, imagen: 'https://icnorteb2c.vteximg.com.br/arquivos/ids/158157-600-600/Leche-Condensada-Pil-397-Gr-1-360.jpg?v=637772457645030000', mensaje: 'LECHE CONDENSADA', precio: 12, descripcion: 'PRODUCTO LÁCTEO'},
+    {id: 2, imagen: 'https://acdn.mitiendanube.com/stores/001/930/794/products/dulce-de-leche-repostero-vacalin-1-kilo-1db60302d0c5c629f517011915416403-480-0.jpg', mensaje: 'DULCE DE LECHE', precio: 16, descripcion: 'PRODUCTO LÁCTEO'},
+    {id: 3, imagen: 'https://farmacorp.com/cdn/shop/files/8445291028050_1200x1200.jpg?v=1723828927', mensaje: 'LECHE EVAPORADA', precio: 11, descripcion: 'PRODUCTO LÁCTEO'},
+    {id: 4, imagen: 'https://icnorteb2c.vteximg.com.br/arquivos/ids/158157-600-600/Leche-Condensada-Pil-397-Gr-1-360.jpg?v=637772457645030000', mensaje: 'LECHE CONDENSADA', precio: 12, descripcion: 'PRODUCTO LÁCTEO'},
+    {id: 5, imagen: 'https://acdn.mitiendanube.com/stores/001/930/794/products/dulce-de-leche-repostero-vacalin-1-kilo-1db60302d0c5c629f517011915416403-480-0.jpg', mensaje: 'DULCE DE LECHE', precio: 16, descripcion: 'PRODUCTO LÁCTEO'},
+    {id: 6, imagen: 'https://farmacorp.com/cdn/shop/files/8445291028050_1200x1200.jpg?v=1723828927', mensaje: 'LECHE EVAPORADA', precio: 11, descripcion: 'PRODUCTO LÁCTEO'},
+    {id: 7, imagen: 'https://icnorteb2c.vteximg.com.br/arquivos/ids/158157-600-600/Leche-Condensada-Pil-397-Gr-1-360.jpg?v=637772457645030000', mensaje: 'LECHE CONDENSADA', precio: 12, descripcion: 'PRODUCTO LÁCTEO'},
+    {id: 8, imagen: 'https://acdn.mitiendanube.com/stores/001/930/794/products/dulce-de-leche-repostero-vacalin-1-kilo-1db60302d0c5c629f517011915416403-480-0.jpg', mensaje: 'DULCE DE LECHE', precio: 16, descripcion: 'PRODUCTO LÁCTEO'},
+    {id: 9, imagen: 'https://farmacorp.com/cdn/shop/files/8445291028050_1200x1200.jpg?v=1723828927', mensaje: 'LECHE EVAPORADA', precio: 11, descripcion: 'PRODUCTO LÁCTEO'},
+    {id: 10, imagen: 'https://icnorteb2c.vteximg.com.br/arquivos/ids/158157-600-600/Leche-Condensada-Pil-397-Gr-1-360.jpg?v=637772457645030000', mensaje: 'LECHE CONDENSADA', precio: 12, descripcion: 'PRODUCTO LÁCTEO'},
+    {id: 11, imagen: 'https://acdn.mitiendanube.com/stores/001/930/794/products/dulce-de-leche-repostero-vacalin-1-kilo-1db60302d0c5c629f517011915416403-480-0.jpg', mensaje: 'DULCE DE LECHE', precio: 16, descripcion: 'PRODUCTO LÁCTEO'},
+    {id: 12, imagen: 'https://farmacorp.com/cdn/shop/files/8445291028050_1200x1200.jpg?v=1723828927', mensaje: 'LECHE EVAPORADA', precio: 11, descripcion: 'PRODUCTO LÁCTEO'}
   ];
 
   private productosSubject = new BehaviorSubject<Product[]>(this.productos);
